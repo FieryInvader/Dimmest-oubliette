3ab5315b68ff44d1f9de1fa88d3d247e6e468231
FieryInvader <greeksky420@gmail.com>
2025-01-09 19:26:55 +0200

    Icons and menus
    More folders and name editing too

--- Game.py
+++ Game.py
@@ -4,40 +4,38 @@ import random
 
 pygame.init()
 
-#Colours
-red = (255,0,0)
-dark_red = (168,10,10)
-shit_yellow = (205,150,0)
-grey = (80,80,80) 
-black = (0,0,0)
-white = (200,200,200)
-
 #apply_blight(self,target,damage,rounds)
 #apply_bleed(self,target,damage,rounds)
 #apply_stun(self,target)
 
+
+#Classes for our heroes
 class Person(pygame.sprite.Sprite):
     def __init__(self,x, y, name, health, critical, dodge, speed):
         #visuals
         pygame.sprite.Sprite.__init__(self)
+        self.name = name
         self.image = pygame.image.load(f"images/heroes/{name}.png")
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         #stats
-        self.hp = health
+        self.max_hp = health
+        self.current_hp = health
         self.stress = 0
         self.crit = critical
         self.dodge = dodge
         self.speed = speed
         self.dmgmod = 0
         self.deathblow_res = 0.67
-        
-        
+            
 class Highwayman(Person):
     def __init__(self, x, y, name):
         #initialize parent class
+        self.hero_class = "Highwayman"
         self.dmg_range = [i for i in range(5,11)]
         super().__init__(x, y, name, 23, 0.05, 0.1, 5)
+        self.abilities=["wicked_slice", "pistol_shot", 
+                        "grapeshot_blast", "open_vein"]
         
     def wicked_slice(self):
         ability_type = 'Attack'
@@ -72,19 +70,14 @@ class Highwayman(Person):
         acc = 0.95
         #apply_bleed(target,dmg,rounds)
         
-    def take_aim(self):
-        ability_type = 'Util'
-        position = [0,1,2,3]
-        self.acc += 0.15
-        self.dmgmod = 2
-        self.crit = self.crit + 0.2
-        self.speed += 2
-        
 class Crusader(Person):
     def __init__(self, x, y, name):
+        self.hero_class = "Crusader"
         #initialize parent class
         self.dmg_range = [i for i in range(6,13)]
         super().__init__(x, y, name, 33, 0.03, 0.05, 1)
+        self.abilities=["smite", "zealous_accusation", 
+                        "stunning_blow", "inspiring_cry"]
         
     def smite(self):
         ability_type = 'Attack'
@@ -110,12 +103,6 @@ class Crusader(Person):
         crit = self.crit 
         acc = 0.9
         
-    def battle_heal(self):
-        ability_type = 'Util'
-        position = [0,1,2,3]
-        target = [0,1,2,3]
-        #heal(target,3)
-        
     def inspiring_cry(self):
         ability_type = 'Util'
         position = [0,1,2,3]
@@ -124,9 +111,12 @@ class Crusader(Person):
         
 class Plague_Doctor(Person):
     def __init__(self, x, y, name):
+        self.hero_class = "Plague_Doctor"
         #initialize parent class
         self.dmg_range = [i for i in range(4,8)]
         super().__init__(x, y, name, 22, 0.02, 0.01, 7)
+        self.abilities=["noxious_blast", "plague_grenade", 
+                        "blinding_gas", "battlefield_medicine"]
         
     def noxious_blast(self):
         ability_type = 'Attack'
@@ -146,7 +136,7 @@ class Plague_Doctor(Person):
         acc = 0.95
         #apply_blight(target,4,3)
         
-    def plague_grenade(self):
+    def blinding_gas(self):
         ability_type = 'Attack'
         position = [2,3]
         target = (2,3)
@@ -154,15 +144,6 @@ class Plague_Doctor(Person):
         crit = 0
         acc = 0.95
         #apply_stun(target)
-        
-    def incision(self):
-        ability_type = 'Attack'
-        position = [0,1,2]
-        target = [0,1]
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 1
-        crit = self.crit * 1.05
-        acc = 0.85
-        #apply_bleed(target,2,3)
         
     def battlefield_medicine(self):
         ability_type = 'Util'
@@ -173,9 +154,12 @@ class Plague_Doctor(Person):
         
 class Vestal(Person):
     def __init__(self, x, y, name):
+        self.hero_class = "Vestal"
         #initialize parent class
         self.dmg_range = [i for i in range(4,9)]
         super().__init__(x, y, name, 24, 0.01, 0.01, 4)
+        self.abilities=["dazzling_light", "divine_grace", 
+                        "divine_comfort", "judgement"]
         
     def dazzling_light(self):
         ability_type = 'Attack'
@@ -200,126 +184,16 @@ class Vestal(Person):
         
     def judgement(self):
         ability_type = 'Attack'
-        position = [2,3]
-        target = (0,1,2,3)
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.5
-        crit = self.crit * 1.05
-        acc = 0.85
-        
-    def illumination(self):
-        ability_type = 'Attack'
         position = [0,1,2,3]
         target = [0,1,2,3]
         dmg = (random.choice(self.dmg_range) + self.dmgmod) * 2
         crit = self.crit * 1.05
         acc = 0.90
 
-class Bandit(Person):
-    def __init__(self,x,y,name):
-        pass
-
-def draw_text(text, text_col, x, y):
-    img = pygame.font.SysFont('Times New Roman', 26).render(text, True, text_col)
-    screen.blit(img, x, y)
-    
-clock = pygame.time.Clock()
-FPS = 60
-screen_width = 1500
-screen_height = 900
-screen = pygame.display
-screen.set_caption('Dimmest oubliet')
-display = screen.set_mode((screen_width,screen_height))
-
-
-#load background
-bg = pygame.image.load('images/dungeon/hallway2.png')
-
-#Calculate tiles that need to fit in screen + 1 for buffer
-tiles = math.ceil(screen_width / bg.get_width()) + 1
-
-#scroll variable for the background scroll
-scroll = 0
-
-#hero init
-dismas = Highwayman(400, 560,'dismas')
-reynauld = Crusader(530,555,'reynauld')
-paracelcus = Plague_Doctor(230,560,'paracelsus')
-junia = Vestal(100,560,'junia')
-
-#creating a group of sprites for heroes
-party = pygame.sprite.Group()
-party.add(dismas)
-party.add(reynauld)
-party.add(paracelcus)
-party.add(junia)
-
-
-
-=======
-class Person():
-    def __init__(self,x, y, name, health, critical,
-                 dodge, speed,position):
-        #visuals
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(f"images/{name}.png")
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        self.position = position
-        #stats
-        self.hp = health
-        self.stress = 0
-        self.crit = critical
-        self.dodge = dodge
-        self.speed = speed
-        self.dmgmod = 0
-        
-class Highwayman(Person):
-    def __init__(self, x, y, name, position):
-        #initialize parent class
-        self.dmg_range = [i for i in range(5,11)]
-        super().__init__(x, y, name, 23 , 0.9, 0.05 , 0.14, 7,position)
-        
-    def wicked_slice(self):
-        position = [0,1]
-        target = [0,1]
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 1.15
-        crit = self.crit + 0.05
-        acc = self.acc  - 0.1
-        
-    def pistol_shot(self):
-        position = [1,2,3]
-        target = [1,2,3]
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.95
-        crit = self.crit + 0.075
-        acc = self.acc - 0.05
-        
-    def grapeshot_blast(self):
-        position = [1,2]
-        target = (0,1,2)
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * -0.4
-        crit = self.crit - 0.09
-        acc = self.acc - 0.2
-        
-    def open_vein(self):
-        position = [0,1,2]
-        target = [0,1]
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * -0.15
-        crit = 0
-        acc = self.acc
-        #apply_bleed(target)
-        
-    def take_aim(self):
-        position = [0,1,2,3]
-        self.acc += 0.15
-        self.dmgmod = 2
-        self.crit = self.crit + 0.2
-        self.speed += 2
-
+#Enemy classes
 class Cutthroat(Person):
     def __init__(self,x,y,name,position):
         super().__init__(x, y, name, 12, 0.725, 0.12,0.025, 3, position)
-
->>>>>>> 28cd73204d5f87db91fff00af67fd9b2d740640d
         
     def Slice_and_dice(self):
         self_tile = [0,1,2]
@@ -350,6 +224,87 @@ class Fusilier(Person):
         dmg = random.choice(range(1,5))
         crit = 0.02
         
+ 
+    
+#Variables
+clock = pygame.time.Clock()
+FPS = 60
+screen_width = 1600
+screen_height = 900
+bottom_panel = 150
+screen = pygame.display
+screen.set_caption('Dimmest oubliette')
+display = screen.set_mode((screen_width,screen_height))
+
+#Colours
+red = (255,0,0)
+dark_red = (168,10,10)
+yellow = (233,200,85)
+grey = (80,80,80) 
+black = (0,0,0)
+white = (200,200,200)
+
+font = pygame.font.SysFont('Times New Roman', 20)      
+ 
+#Text creation
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    display.blit(img, (x, y))
+    
+def draw_panel():
+    #panels
+    left_panel = pygame.image.load("images/panels/left_decor.png")
+    right_panel = pygame.image.load("images/panels/right_decor.png")
+    panel_hero = pygame.image.load("images/panels/panel_hero.png")
+    panel_banner = pygame.image.load("images/panels/panel_banner.png")
+    display.blit(left_panel, (0, 575))
+    display.blit(right_panel, (1380, 575))
+    display.blit(panel_hero, (220, 700))
+    display.blit(panel_banner, (194, 580))
+    
+
+def draw_hero(hero):
+    icon = pygame.image.load(f"images/heroes/{hero.name}_icon.png")
+    display.blit(icon, (245, 605))
+    draw_text(hero.name, font, yellow, 320, 620)
+    draw_text(hero.hero_class, font, grey, 320, 640)
+    next_icon = 0 
+    for ability in hero.abilities:
+        icon = pygame.image.load(f"images/{hero.hero_class}/{ability}.png")
+        display.blit(icon, (445 + next_icon, 607))
+        next_icon += 62
+    icon = pygame.image.load("images/heroes/ability_move.png")
+    display.blit(icon, (445 + next_icon, 607))
+    next_icon += 62
+    icon = pygame.image.load("images/heroes/ability_pass.png")
+    display.blit(icon, (445 + next_icon, 607))       
+    
+
+
+#load background
+bg = pygame.image.load('images/dungeon/hallway2.png')
+
+#Calculate tiles that need to fit in screen + 1 for buffer
+tiles = math.ceil(screen_width / bg.get_width()) + 1
+
+#scroll variable for the background scroll
+scroll = 0
+
+#hero init
+dismas = Highwayman(400, 400, 'Dismas')
+reynauld = Crusader(530 ,390, 'Reynauld')
+paracelcus = Plague_Doctor(230, 400, 'Paracelsus')
+junia = Vestal(100, 400, 'Junia')
+
+#creating a group of sprites for heroes
+party = pygame.sprite.Group()
+party.add(dismas)
+party.add(reynauld)
+party.add(paracelcus)
+party.add(junia)
+
+
+        
 COMBAT = pygame.USEREVENT + 1
 tile_size = 1000
 party_position = 0
@@ -379,7 +334,8 @@ while run:
     if map_tiles[current_tile] == 1:
         pygame.event.post(pygame.event.Event(COMBAT))
         
-    
+    draw_panel()
+    draw_hero(dismas)
     #When we have scrolled past the screen reset the queue
     if abs(scroll) > bg.get_width():
         scroll = 0
