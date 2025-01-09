f62ee36855e4802941e9ceca14ef242445008f1c
FieryInvader <greeksky420@gmail.com>
2025-01-09 15:19:30 +0200

    Classes Added
    Will probably rework soon

--- Game.py
+++ Game.py
@@ -4,18 +4,223 @@ import random
 
 pygame.init()
 
-class Square(pygame.sprite.Sprite):     #Note: create specific hero classes for abilities and stuff
-    def __init__(self, x, y,name):
+#Colours
+red = (255,0,0)
+dark_red = (168,10,10)
+shit_yellow = (205,150,0)
+grey = (80,80,80) 
+black = (0,0,0)
+white = (200,200,200)
+
+#apply_blight(self,target,damage,rounds)
+#apply_bleed(self,target,damage,rounds)
+#apply_stun(self,target)
+
+class Person(pygame.sprite.Sprite):
+    def __init__(self,x, y, name, health, critical, dodge, speed):
+        #visuals
         pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(f"images/heroes/{name}.png")
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-    
-    # def update(self):
-    #     if key[pygame.K_d]:
-    #         self.rect.move_ip(1, 0)
-    #     elif key[pygame.K_a]:
-    #         self.rect.move_ip(-1,0)
+        #stats
+        self.hp = health
+        self.stress = 0
+        self.crit = critical
+        self.dodge = dodge
+        self.speed = speed
+        self.dmgmod = 0
+        self.deathblow_res = 0.67
+        
+        
+class Highwayman(Person):
+    def __init__(self, x, y, name):
+        #initialize parent class
+        self.dmg_range = [i for i in range(5,11)]
+        super().__init__(x, y, name, 23, 0.05, 0.1, 5)
+        
+    def wicked_slice(self):
+        ability_type = 'Attack'
+        position = [0,1,2]
+        target = [0,1]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 1.15
+        crit = self.crit * 1.05
+        acc = 0.85
+        
+    def pistol_shot(self):
+        ability_type = 'Attack'
+        position = [1,2,3]
+        target = [1,2,3]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.85
+        crit = self.crit * 1.075
+        acc = 0.85
+        
+    def grapeshot_blast(self):
+        ability_type = 'Attack'
+        position = [1,2]
+        target = (0,1,2)
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.5
+        crit = self.crit * 0.91
+        acc = 0.75
+        
+    def open_vein(self):
+        ability_type = 'Attack'
+        position = [0,1,2]
+        target = [0,1]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.85
+        crit = self.crit * 1
+        acc = 0.95
+        #apply_bleed(target,dmg,rounds)
+        
+    def take_aim(self):
+        ability_type = 'Util'
+        position = [0,1,2,3]
+        self.acc += 0.15
+        self.dmgmod = 2
+        self.crit = self.crit + 0.2
+        self.speed += 2
+        
+class Crusader(Person):
+    def __init__(self, x, y, name):
+        #initialize parent class
+        self.dmg_range = [i for i in range(6,13)]
+        super().__init__(x, y, name, 33, 0.03, 0.05, 1)
+        
+    def smite(self):
+        ability_type = 'Attack'
+        position = [0,1]
+        target = [0,1]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 1
+        crit = self.crit
+        acc = 0.85
+        
+    def zealous_accusation(self):
+        ability_type = 'Attack'
+        position = [0,1]
+        target = (0,1)
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.5
+        crit = self.crit - 0.04
+        acc = 0.85
+        
+    def stunning_blow(self):
+        ability_type = 'Attack'
+        position = [0,1]
+        target = [0,1]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * -0.5
+        crit = self.crit 
+        acc = 0.9
+        
+    def battle_heal(self):
+        ability_type = 'Util'
+        position = [0,1,2,3]
+        target = [0,1,2,3]
+        #heal(target,3)
+        
+    def inspiring_cry(self):
+        ability_type = 'Util'
+        position = [0,1,2,3]
+        target = [0,1,2,3]
+        #stressheal(target,1)
+        
+class Plague_Doctor(Person):
+    def __init__(self, x, y, name):
+        #initialize parent class
+        self.dmg_range = [i for i in range(4,8)]
+        super().__init__(x, y, name, 22, 0.02, 0.01, 7)
+        
+    def noxious_blast(self):
+        ability_type = 'Attack'
+        position = [1,2,3]
+        target = [0,1]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.2
+        crit = self.crit * 1.05
+        acc = 0.95
+        #apply_blight(target,5,3)
+        
+    def plague_grenade(self):
+        ability_type = 'Attack'
+        position = [2,3]
+        target = (2,3)
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.1
+        crit = self.crit * 1
+        acc = 0.95
+        #apply_blight(target,4,3)
+        
+    def plague_grenade(self):
+        ability_type = 'Attack'
+        position = [2,3]
+        target = (2,3)
+        dmg = 0
+        crit = 0
+        acc = 0.95
+        #apply_stun(target)
+        
+    def incision(self):
+        ability_type = 'Attack'
+        position = [0,1,2]
+        target = [0,1]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 1
+        crit = self.crit * 1.05
+        acc = 0.85
+        #apply_bleed(target,2,3)
+        
+    def battlefield_medicine(self):
+        ability_type = 'Util'
+        position = [2,3]
+        target = [0,1,2,3]
+        #heal(target,1)
+        #cure(target)
+        
+class Vestal(Person):
+    def __init__(self, x, y, name):
+        #initialize parent class
+        self.dmg_range = [i for i in range(4,9)]
+        super().__init__(x, y, name, 24, 0.01, 0.01, 4)
+        
+    def dazzling_light(self):
+        ability_type = 'Attack'
+        position = [1,2,3]
+        target = [0,1,2]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.25
+        crit = self.crit * 1.05
+        acc = 0.90
+        #apply_stun(target)
+        
+    def divine_grace(self):
+        ability_type = 'Util'
+        position = [2,3]
+        target = [0,1,2,3]
+        #heal(target,5)
+        
+    def divine_comfort(self):
+        ability_type = 'Util'
+        position = [2,3]
+        target = (0,1,2,3)
+        #heal(target,5)
+        
+    def prayer(self):
+        ability_type = 'Util'
+        position = [2,3]
+        target = [0,1,2,3]
+        #if target.stress >= 5
+            #stressheal(target,2)
+        
+    def illumination(self):
+        ability_type = 'Attack'
+        position = [0,1,2,3]
+        target = [0,1,2,3]
+        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 1.5
+        crit = self.crit * 1.05
+        acc = 0.90
 
+class Bandit(Person):
+    def __init__(self,x,y,name):
+        pass
+
+def draw_text(text, text_col, x, y):
+    img = pygame.font.SysFont('Times New Roman', 26).render(text, True, text_col)
+    screen.blit(img, x, y)
+    
 clock = pygame.time.Clock()
 FPS = 60
 screen_width = 1500
@@ -35,10 +240,10 @@ tiles = math.ceil(screen_width / bg.get_width()) + 1
 scroll = 0
 
 #hero init
-dismas = Square(400, 560,'dismas')
-reynauld = Square(530,555,'reynauld')
-paracelcus = Square(230,560,'paracelsus')
-junia = Square(100,560,'junia')
+dismas = Highwayman(400, 560,'dismas')
+reynauld = Crusader(530,555,'reynauld')
+paracelcus = Plague_Doctor(230,560,'paracelsus')
+junia = Vestal(100,560,'junia')
 
 #creating a group of sprites for heroes
 party = pygame.sprite.Group()
@@ -48,70 +253,7 @@ party.add(paracelcus)
 party.add(junia)
 
 
-class Person():
-    def __init__(self,x, y, name, health, accuracy, critical, damage,
-                 dodge, speed):
-        #visuals
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(f"images/{name}.png")
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        #stats
-        self.hp = health
-        self.stress = 0
-        self.acc = accuracy
-        self.crit = critical
-        self.dmg = damage
-        self.dodge = dodge
-        self.speed = speed
-        self.dmgmod = 0
-        
-class Highwayman(Person):
-    def __init__(self, x, y, name):
-        #initialize parent class
-        self.dmg_range = [i for i in range(5,11)]
-        super().__init__(x, y, name, 23 , 0.9, 0.05 , self.dmg_range, 0.14, 7)
-        
-    def wicked_slice(self):
-        position = [0,1]
-        target = [0,1]
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 1.15
-        crit = self.crit + 0.05
-        acc = self.acc  - 0.1
-        
-    def pistol_shot(self):
-        position = [1,2,3]
-        target = [1,2,3]
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * 0.95
-        crit = self.crit + 0.075
-        acc = self.acc - 0.05
-        
-    def grapeshot_blast(self):
-        position = [1,2]
-        target = (0,1,2)
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * -0.4
-        crit = self.crit - 0.09
-        acc = self.acc - 0.2
-        
-    def open_vein(self):
-        position = [0,1,2]
-        target = [0,1]
-        dmg = (random.choice(self.dmg_range) + self.dmgmod) * -0.15
-        crit = 0
-        acc = self.acc
-        #apply_bleed(target)
-        
-    def take_aim(self):
-        position = [0,1,2,3]
-        self.acc += 0.15
-        self.dmg = 2
-        self.crit = self.crit + 0.2
-        self.speed += 2
 
-=======
-class Bandit(Person):
-    def __init__(self,x,y,name):
-        pass
         
     
 tile_size = 1000
